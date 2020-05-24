01f9fd0d67787a9145c60b2e385ac3bfe7a6f85c
Александр Тихонович <rulers.oftheinfinite@mail.ru>
2020-05-25 00:27:02 +0300

    Updates

---
+++ precache-manifest.5fbc79a4f12463d1be64b2dfabada9a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75daa6314b1af7675ff562f6b768326b",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "ff6b9c5bdf922c1b5af9",
+    "url": "/store/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ae21751ca69ba555a442",
+    "url": "/store/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ff6b9c5bdf922c1b5af9",
+    "url": "/store/static/js/2.5c69f0b9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/store/static/js/2.5c69f0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae21751ca69ba555a442",
+    "url": "/store/static/js/main.caff2634.chunk.js"
+  },
+  {
+    "revision": "00870d63df9e85a5860f",
+    "url": "/store/static/js/runtime-main.19bc2dcc.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/store/static/media/persik.4e1ec840.png"
+  }
+]);
